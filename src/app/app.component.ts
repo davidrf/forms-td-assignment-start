df5e9839513e186f40292c194cfa2bf400d4ce94
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2017-12-02 13:50:26 -0500

    complete assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signUpForm: NgForm;
+  defaultSubscription = 'Advanced';
+  subscriptions = ['Basic', 'Advanced', 'Pro']
 
+  onSubmit() {
+    console.log(this.signUpForm.value);
+  }
 }
